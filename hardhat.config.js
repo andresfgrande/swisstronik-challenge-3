5662e04524dd009513e77bb799c63712a4cc1b40
Andrés Grande <andres.grande@somosinapsis.com>
2023-08-31 16:24:39 +0200

    Add dotenv variable

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,19 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-ethers");
+require('dotenv').config()
+
 module.exports = {
   solidity: "0.8.19",
     networks: {
     localhost: {
       url: "http://127.0.0.1:8545"  
     },
+    swisstronikTestnet: {
+      url: "https://json-rpc.testnet.swisstronik.com",  
+      accounts: [process.env.PRIVATE_KEY],  
+      chainId: 1291,  
+    },
  
   },
 };
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,15 @@
 const hre = require("hardhat");
 
 async function main() {
+  
   const [deployer] = await ethers.getSigners();
 
-  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Deploying token contract with the account:", deployer.address);
 
   const token = await ethers.deployContract("MySwissToken");
 
   console.log("New Swisstronik token address:", await token.getAddress());
+  
 }
 
 main()
